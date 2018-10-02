0b3f3c4a28b39c702602ea055be0bac5e554c1f9
Flavio Magalhães <flaviomagalhaest@gmail.com>
2018-10-01 22:18:52 -0300

    Test fix

--- app/tests/buildObjectUser.test.js
+++ app/tests/buildObjectUser.test.js
@@ -2,7 +2,7 @@ const controller = require('../controller/moochimpController');
 
 it('expect moodle object user populate', () => {
     var users = [{"id":"883162de08ec1bd75d151e6d4124b8ea","email_address":"test@outlook.com","merge_fields":{"FNAME":"Leon","LNAME":"Freitas","ADDRESS":"","PHONE":"","MMERGE5":"","MMERGE6":"1992-02-22","MMERGE7":"9999999","MMERGE8":"","MMERGE9":"Desenvolvedor","MMERGE10":"","MMERGE11":"","MMERGE12":"2","MMERGE13":"2018-09-25 14:09:58"}}];
-    params = {"moodle":{"body":{"username":{"email_address":""},"password":["Fcamara@",{"merge_fields":{"MMERGE6":""}}],"firstname":{"merge_fields":{"FNAME":""}},"lastname":{"merge_fields":{"LNAME":""}},"email":{"email_address":""}}}};
-    result = [{"username":"test@outlook.com","password":"Fcamara@1992-02-22","firstname":"Leon","lastname":"Freitas","email":"test@outlook.com"}];
+    params = {"moodle":{"body":{"username":{"email_address":""},"password":["Fcamara@",{"merge_fields":{"FNAME":""}}],"firstname":{"merge_fields":{"FNAME":""}},"lastname":{"merge_fields":{"LNAME":""}},"email":{"email_address":""}}}};
+    result = [{"username":"test@outlook.com","password":"Fcamara@leon","firstname":"leon","lastname":"freitas","email":"test@outlook.com"}];
     expect(controller.buildObjectUser(users)).toEqual(result);
 });
